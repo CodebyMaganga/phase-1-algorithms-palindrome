1047d204c7314163bd651761c0a0b432a74e8414
Maganga Mwambonu <codebymaganga@gmail.com>
2023-10-13 11:53:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// Write your algorithm here
+  function isPalindrome(word) {
+    
+    const originalWord = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  
+   
+    const reversedWord = originalWord.split('').reverse().join('');
+  
+    if(originalWord === reversedWord){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  
+  // Example usage:
+  const word = "racecar";
+  if (isPalindrome(word)) {
+    console.log(`${word} is a palindrome.`);
+  } else {
+    console.log(`${word} is not a palindrome.`);
+  }
+  
+
+
 
 /* 
   Add your pseudocode here
+  1.create an iteration that goes thru the orinal word beginning to end 
+  2.reverse the word and iterate thru it
+  3.Compare the two
 */
-
+  
 /*
   Add written explanation of your solution here
+  1.We declare a variable that has a function that removes all characters in the string that are not numbers or alphabets
+  2.We then declare another variable that holds the reversed string
+  3.We compare if this two variables are the same
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +52,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+console.log(isPalindrome('racecar'))
